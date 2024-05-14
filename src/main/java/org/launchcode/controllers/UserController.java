e8e45132f003532f7cc64f9b8e0576c6961291e0
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-05-13 20:36:37 -0500

    Chapter 15 Studio complete

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,34 +1,35 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/add")
-    public String displayAddUserForm() {
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute( new User());
         return "user/add";
     }
 
-    @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
+    @PostMapping("/add")
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        System.out.println(errors);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("user", user);
+//        model.addAttribute("errors", errors);
+        if (!user.getPassword().equals(verify) || errors.hasErrors()) {
             model.addAttribute("error", "Passwords do not match");
+            if (!user.getPassword().equals(verify)) {
+                model.addAttribute("displayError", true);
+            }
             return "user/add";
         }
-
+        return "user/index";
     }
-
-
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,19 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+    
+    @Email(message = "Must be a valid email")
     private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 15,message = "Password must be at least 6 characters")
     private String password;
 
     public User() {
